f0bb1b7eeec6f3b6f68f2f44682d01e84b80130d
shuozhang-test <2638401898@qq.com>
2018-12-20 23:45:46 +0800

    first add

---
+++ src/components/HeaderBar/HeaderBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header-bar">
+    <text>头部</text>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .header-bar {
+      height: 80px;
+      width: 750px;
+      background: red;
+  }
+</style>
---
+++ src/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+      <header-bar />
+  </div>
+</template>
+
+<script>
+  import HeaderBar from './components/HeaderBar/HeaderBar';
+  export default {
+    components: {
+      HeaderBar
+    },
+    data () {
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .wrapper {
+    width: 100vw;
+    height: 100vh;
+    justify-content: flex-start;
+  }
+</style>
